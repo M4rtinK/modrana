a6d3456f90012d13af46a7cdfd3bcd667f5f5b39
xkolman2 <xkolman2@0858a4d0-ffff-46e5-938e-62b5ecb34222>
2010-07-19 22:41:42 +0000

    remove another unnecessary netbeans import
    git-svn-id: https://nlp.fi.muni.cz/svn/gps_navigace/trunk@153 0858a4d0-ffff-46e5-938e-62b5ecb34222

--- modules/mod_tracklogManager.py
+++ modules/mod_tracklogManager.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #---------------------------------------------------------------------------
-from gpod.gpod import LT_OBJDIR
 from base_module import ranaModule
 import math
 import os
