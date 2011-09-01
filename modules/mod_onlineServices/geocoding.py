0e87dda533461a4c146a9c024054c99d6cd3744f
xkolman2 <xkolman2@0858a4d0-ffff-46e5-938e-62b5ecb34222>
2011-09-01 13:45:25 +0000

    make descriptions multiline
    git-svn-id: https://nlp.fi.muni.cz/svn/gps_navigace/trunk@550 0858a4d0-ffff-46e5-938e-62b5ecb34222

--- modules/mod_onlineServices/geocoding.py
+++ modules/mod_onlineServices/geocoding.py
@@ -1,12 +1,32 @@
 """multi source geocoding"""
 from point import Point
+import re
+
+class GeoPyPoint(Point):
+  """
+  * the GeoPy geocoding results return composite place names contaning
+  the address components delimited by ','
+  * this point Point subclass just splits the part before the first ','
+  and returns it when the getName method is called
+  * also, it outpust a description text where all ',' are replaced by newlines
+  """
+  def __init__(self, lat, lon, placeText):
+    Point.__init__(self, lat, lon, message=placeText)
+    self.name=placeText.split(',')[0]
+    self.description=re.sub(',','\n',placeText) # replace separators with newlines
+
+  def getName(self):
+    return self.name
+
+  def getDescription(self):
+    return self.description
 
 def _places2points(places):
   """convert place tupples to modRana points"""
   points = []
   for place in places:
     text, (lat,lon) = place
-    points.append(Point(lat,lon, message=text))
+    points.append(GeoPyPoint(lat,lon, text))
   return points
 
 def geocode(address):
@@ -18,7 +38,7 @@ def geocode(address):
   except Exception, e:
     print("geocoding exception:\n", e)
     return []
-  
+
 #def wikipediaSearch(query):
 #  from geopy import geocoders
 ##  wiki = geocoders.MediaWiki("http://wikipedia.org/wiki/%s")
