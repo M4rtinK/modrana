768c39c0fba9c862c45417fd06bd69c93a994cb4
xkolman2 <xkolman2@0858a4d0-ffff-46e5-938e-62b5ecb34222>
2012-03-17 18:47:03 +0000

    move configobj
    git-svn-id: https://nlp.fi.muni.cz/svn/gps_navigace/trunk@690 0858a4d0-ffff-46e5-938e-62b5ecb34222

--- core/configobj/configobj.py
+++ core/configobj/configobj.py
@@ -2155,7 +2155,7 @@ class ConfigObj(Section):
             if preserve_errors:
                 # We do this once to remove a top level dependency on the validate module
                 # Which makes importing configobj faster
-                from validate import VdtMissingValue
+                from core.configobj.validate import VdtMissingValue
                 self._vdtMissingValue = VdtMissingValue
                 
             section = self
