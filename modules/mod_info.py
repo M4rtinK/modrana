22aa7b67fb5cc391b7f96f799593c8bd9fb7bcb6
Martin Kolman <martin.kolman@gmail.com>
2012-05-14 12:08:22 +0200

    improved "about" text

--- modules/mod_info.py
+++ modules/mod_info.py
@@ -39,7 +39,43 @@ class info(ranaModule):
     return "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=martin%2ekolman%40gmail%2ecom&lc=CZ&item_name=The%20modRana%20project&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"
 
   def getDiscussionUrls(self):
-    return [("http://talk.maemo.org/showthread.php?t=58861","Discussion@TMO")]
+    """
+    return a list of modRana-relevant discussions, with the most relevant discussion on top
+    """
+    return [("http://talk.maemo.org/showthread.php?t=58861","talk.maemo.org thread")]
+
+  def getMainDiscussionUrl(self):
+    """
+    return Url to the most relevant modRana discussion
+    """
+    return self.getDiscussionUrls()[0]
+
+  def getWebsiteUrl(self):
+    """
+    return Url to the modRana website
+    """
+    return "http://www.modrana.org"
+
+  def getSourceRepositoryUrl(self):
+    return "https://github.com/M4rtinK/modrana"
+
+  def getEmailAddress(self):
+    """
+    return the project email address
+    """
+    return "modrana@gmail.com"
+
+  def getAboutText(self):
+    www = self.getWebsiteUrl()
+    email = self.getEmailAddress()
+    source = self.getSourceRepositoryUrl()
+    discussion, name = self.getDiscussionUrls()[0]
+    text= "<p><b>main developer:</b> Martin Kolman</p>"
+    text+= '<p><b>email</b>: <a href="mailto:%s">%s</a></p>' % (email, email)
+    text+= '<p><b>www</b>: <a href="%s">%s</a></p>' % (www, www)
+    text+= '<p><b>source</b>:\n<a href="%s">%s</a></p>' % (source, source)
+    text+= '<p><b>discussion</b>: check <a href="%s">%s</a></p>' % (discussion, name)
+    return text
 
   def drawMenu(self, cr, menuName, args=None):
     if menuName == 'infoAbout':
