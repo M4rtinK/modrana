f031f4b8bf4ba4f37e76fdabec97e8da392d297d
Martin Kolman <martin.kolman@gmail.com>
2012-09-08 12:08:01 +0200

    use a thread for starting up the monav-server process & block until it is started up; only report that the server was start/stopped, if it actually happened

--- modules/mod_route/monav_support.py
+++ modules/mod_route/monav_support.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #---------------------------------------------------------------------------
+from threading import Thread
 import time
 import subprocess
 import signal
@@ -28,38 +29,61 @@ import monav
 # Marble stores monav data like this on the N900:
 # /home/user/MyDocs/.local/share/marble/maps/earth/monav/motorcar/europe/czech_republic
 
-MONAV_BINARY_PATH = 'modules/mod_route/monav_arm7/monav-server'
-# TODO: other architectures
-
 class Monav:
-  def __init__(self):
+  def __init__(self, monavBinaryPath):
     self.monavServer = None
     self.connection = None
+    self.monavServerBinaryPath = monavBinaryPath
 
   def startServer(self, port=None):
     print('monav_support: starting Monav server')
+    started = False
     try:
       # first check if monav server is already running
       try:
         self.connection = monav.TcpConnection()
         print('monav_support: server already running')
       except Exception, e:
-        print('monav_support: server not yet running')
-        self.monavServer = subprocess.Popen(
-          "%s" % MONAV_BINARY_PATH
-        )
+        print('monav_support: starting monav server in:')
+        print(self.monavServerBinaryPath)
+
+        def _startServer(self):
+          self.monavServer = subprocess.Popen(
+            "%s" % self.monavServerBinaryPath
+          )
+
+        t = Thread(target=_startServer, args=[self], name='asynchronous monav-server start')
+        t.setDaemon(True) # we need that the thread dies with the program
+        t.start()
+        timeout = 5 # in s
+        sleepTime = 0.1 # in s
+        startTimestamp = time.time()
+        elapsed = 0
+        # wait up to timeout seconds for the server to start
+        # and then return
+        while elapsed < timeout:
+          if self.monavServer:
+            break
+          time.sleep(sleepTime)
+          elapsed = time.time() - startTimestamp
+        started = True
+
         self.connection = monav.TcpConnection()
         # TODO: use other port than 8040 ?, check out tileserver code
     except Exception, e:
       print('monav_support: starting Monav server failed')
       print(e)
-    print('monav_support: Monav server started')
+    if started:
+      print('monav_support: Monav server started')
 
   def stopServer(self):
     print('monav_support: stopping Monav server')
+    stopped = False
     try:
       if self.monavServer:
-        os.kill(self.monavServer.pid, signal.SIGKILL)
+        #os.kill(self.monavServer.pid, signal.SIGKILL)
+        self.monavServer.terminate()
+        stopped = True
       else:
         print('monav_support: no Monav server process found')
     except Exception, e:
@@ -67,7 +91,8 @@ class Monav:
       print(e)
     self.monavServer = None
     self.connection = None
-    print('monav_support: Monav server stopped')
+    if stopped:
+      print('monav_support: Monav server stopped')
 
   def serverRunning(self):
     if self.monavServer:
@@ -80,13 +105,12 @@ class Monav:
     # check if Monav server is running
     if not self.serverRunning():
       self.startServer() # start the server
-
     print('monav: starting route search')
     start = time.clock()
     result = monav.get_route(dataDirectory,
                              waypoints,
                              connection=self.connection)
-    print('monav: search finished in %1.2 ms'  % (1000 * (time.clock() - start)) )
+    print('monav: search finished in %1.2f ms'  % (1000 * (time.clock() - start)) )
     return result
 
   def monavDirectionsAsync(self, start, destination, callback, key):
