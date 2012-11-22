3969e1eb7dd92d167356540ebf47e7ce0a8239e3
Martin Kolman <martin.kolman@gmail.com>
2012-11-22 13:27:08 +0100

    fix map tile filtering ignoring options on startup

--- modules/mod_mapTiles.py
+++ modules/mod_mapTiles.py
@@ -139,7 +139,9 @@ class MapTiles(ranaModule):
     self.modrana.watch('currentTheme', self._updateTileFilteringCB)
     self.modrana.watch('invertMapTiles', self._updateTileFilteringCB)
     # check if tile filtering should be enabled with current theme
+    invert = self.get('invertMapTiles', False)
     theme = self.get('currentTheme', 'default')
+    self._updateTileFilteringCB(key="invertMapTiles", oldValue=None, newValue=invert)
     self._updateTileFilteringCB(key="currentTheme", oldValue=None, newValue=theme)
 
   def getTile(self, layer, z, x, y):
