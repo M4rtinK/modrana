3708f19ced214eee2a2746c73e0a373642e4bc4a
Martin Kolman <martin.kolman@gmail.com>
2013-04-27 12:47:50 +0200

    Initial device modules for Android and BB10

---
+++ modules/device_modules/device_android.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# A Neo FreeRunner modRana device-specific module.
+# It is a basic modRana module, that has some special features
+# and is loaded only on the correpsponding device.
+#----------------------------------------------------------------------------
+# Copyright 2010, Martin Kolman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#---------------------------------------------------------------------------
+from base_device_module import DeviceModule
+
+def getModule(m,d,i):
+  return Android(m,d,i)
+
+class Android(DeviceModule):
+  """A modRana device-specific module for Android"""
+  
+  def __init__(self, m, d, i):
+    DeviceModule.__init__(self, m, d, i)
+    self.tempUnfullscreen = False
+
+  def getDeviceIDString(self):
+    return "android"
+
+  def getDeviceName(self):
+    return "Android device"
+
+  def getWinWH(self):
+    return 480,800
+
+  def startInFullscreen(self):
+    return True
+
+  def getSupportedGUIModuleIds(self):
+    return ["QML:harmattan", "QML:indep"]
+
+  def getLocationType(self):
+    return "qt_mobility"
+
+  def screenBlankingControlSupported(self):
+    """
+    Screen blanking support is handled through Qt Mobility
+    """
+    return True
+
+  def pauseScreenBlanking(self):
+    """
+    inhibit screen blanking
+    """
+    from QtMobility.SystemInfo import QSystemScreenSaver
+
+    if self.qScreenSaver:
+      QSystemScreenSaver.setScreenSaverInhibit(self.qScreenSaver)
+    else:
+      self.qScreenSaver = QSystemScreenSaver()
+
+  def needsQuitButton(self):
+    """due to no window decoration, own quit button
+    might be needed"""
+    return True
