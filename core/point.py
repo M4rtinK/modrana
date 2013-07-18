5d0a8f62116365819b399e432ce855b72ffa9651
Martin Kolman <martin.kolman@gmail.com>
2013-07-18 13:12:59 +0200

    Use properties for a improved Point interface

--- core/point.py
+++ core/point.py
@@ -2,11 +2,12 @@
 
 class Point(object):
   """a point"""
-  def __init__(self, lat, lon, elevation=None, message=None):
+  def __init__(self, lat, lon, elevation=None, name=None, message=None):
     self.lat = lat
     self.lon = lon
     self.elevation = elevation # should be in meters
-    self.message = message
+    self._name = name
+    self._message = message
 
   def __unicode__(self):
     if self.getElevation() is None:
@@ -14,10 +15,39 @@ class Point(object):
     else:
       elev = "%f m" % self.getElevation()
 
-    return '%f,%f elevation: %s "%s"' % (self.lat, self.lon, elev, self.getMessage())
+    return '%f,%f elev: %s "%s:%s"' % (self.lat, self.lon, elev, self.name, self.message)
 
   def __str__(self):
     return unicode(self).encode('utf-8')
+
+  @property
+  def name(self):
+    if self._name:
+      return self._name
+    elif self._message:
+      return self._message.split('\n',1)[0]
+    else:
+      return None
+
+  @name.setter
+  def name(self, value):
+    self._name = value
+
+  @property
+  def message(self):
+    return self._message
+
+  @message.setter
+  def message(self, value):
+    self._message = value
+
+  @property
+  def description(self):
+    return self._name
+
+  @description.setter
+  def description(self, value):
+    self._message = value
 
   def getLL(self):
     return self.lat,self.lon
@@ -35,7 +65,7 @@ class Point(object):
     self.elevation = elevation
 
   def getLLEM(self):
-    return self.lat,self.lon, self.elevation, self.message
+    return self.lat,self.lon, self.elevation, self._message
 
   def getElevation(self):
     return self.elevation
@@ -44,21 +74,21 @@ class Point(object):
     self.elevation = elevation
 
   def getMessage(self):
-    return self.message
+    return self._message
 
   def setMessage(self, message):
-    self.message = message
+    self._message = message
 
   def getName(self):
-    return self.message
+    return self._message
 
   def getDescription(self):
-    return self.message
+    return self._message
 
   def getAbstract(self):
     """a short single line point description"""
-    if self.message:
-      return self.message.split('\n',1)[0]
+    if self._message:
+      return self._message.split('\n',1)[0]
 
   def getUrls(self):
     """get a list of (Url, Url description) tuples corresponding to the point"""
