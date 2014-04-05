7455687584a6045e8c54f4b9b879b4eeb54c14d6
Martin Kolman <martin.kolman@gmail.com>
2014-04-05 20:01:46 +0200

    Improve pool shutdown possibilities

--- core/pool.py
+++ core/pool.py
@@ -43,6 +43,10 @@ class ThreadPool(object):
         # calling object() we get an unique id that
         # shouldn't get mixed up with any other work item
 
+        # we need to keep track of our workers so we can join
+        # them in some pool shutdown scenarios
+        self._workers = []
+
         self._terminator = object()
         self._shutdownLock = threading.RLock()
         self._shutdown = False
@@ -71,7 +75,8 @@ class ThreadPool(object):
                 name=threadName,
                 target=self._worker
             )
-            threads.threadMgr.add(thread)
+            # save the thread ID
+            self._workers.append(threads.threadMgr.add(thread))
 
     def _worker(self):
         """Worker method running in a ModRanaThread"""
@@ -80,7 +85,7 @@ class ThreadPool(object):
             if item == self._terminator:
                 # shutdown
                 self._workQueue.task_done()
-                return
+                break
             else:
                 fn, args, kwargs = item
                 fn(*args, **kwargs)
@@ -97,29 +102,61 @@ class ThreadPool(object):
                 # normal work items have priority 1
                 self._workQueue.put((1, (fn, args, kwargs)))
 
-    def shutdown(self, now=False, join=False):
-        """Shutdown the thread pool"""
+    def shutdown(self, now=False, join=False, async=True, callback=None):
+        """Shutdown the lifo thread pool"""
         with self._shutdownLock:
-            self._shutdown = True
+            if self._shutdown:
+                # shutdown already in progress or not running
+                return False
+            else:
+                self._shutdown = True
         # after shutdown is set, all submit calls
         # will throw RuntimeError, so there should be
         # no danger shutdown requests will get mixed up with
         # work requests (not that it mattered BTW, as long as
         # nr shutdown requests >= nr of threads)
 
-        priority = 1
+        # if the queue is bounded, the shutdown function may block
+        # so run it in a thread by default, with the possibility of
+        # triggering a callback once it is done
+        if async:
+            call = lambda : self._shutdownWrapper(now, join, callback)
+            t = threads.ModRanaThread(name=self.name+"Shutdown",
+                                      target=call)
+            threads.threadMgr.add(t)
+        else:
+            self._shutdownWrapper(now, join, callback)
+
+        return True
+
+
+    def _shutdownWrapper(self, now, join, callback):
+        self._shutdownHandler(now, join)
+        # is there a callback for when we are done ?
+        if callback:
+            # call it !
+            callback()
+
+    def _shutdownHandler(self, now, join):
+        priority = 2
         if now:
             priority = 0
-        # tuples with index 1 will get delivered after
-        # all work requests are processed, items
-        # with index 0 will get delivered before any
+        # tuples with index 2 will get delivered after
+        # all work requests with index 1 are processed,
+        # items with index 0 will get delivered before any
         # work requests (so we can shutdown even if we have
         # some unprocessed items in the queue)
         for index in range(1, self._maxThreads+1):
             self._workQueue.put((priority, self._terminator))
         # if requested, wait for the pool to shutdown
+        # import pdb;pdb.set_trace()
         if join:
-            self._workQueue.join()
+            # we can't just coin the queue as there might still
+            # be work items in it during an explicit shutdown
+            for threadId in self._workers:
+                threads.threadMgr.wait(threadId)
+        # clean the worker list, just in case
+        self._workers = []
 
 
 class LifoThreadPool(ThreadPool):
@@ -139,16 +176,7 @@ class LifoThreadPool(ThreadPool):
         # tasks once the task becomes full as the work queue
         return queues.LeakyLifoQueue(maxsize=self._queueSize, leak=self._leak)
 
-    def shutdown(self, now=False, join=False):
-        """Shutdown the lifo thread pool"""
-        with self._shutdownLock:
-            self._shutdown = True
-        # after shutdown is set, all submit calls
-        # will throw RuntimeError, so there should be
-        # no danger shutdown requests will get mixed up with
-        # work requests (not that it mattered BTW, as long as
-        # nr shutdown requests >= nr of threads)
-
+    def _shutdownHandler(self, now, join):
         # the stack queue actually only supports shutting
         # down at once so now == False doesn't have any effect
         for index in range(1, self._maxThreads+1):
@@ -156,15 +184,3 @@ class LifoThreadPool(ThreadPool):
         # if requested, wait for the pool to shutdown
         if join:
             self._workQueue.join()
-
-
-
-
-
-
-
-
-
-
-
-
