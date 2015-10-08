9b42fd2d725d2d029c87a3c9cc25727e55f54c70
Martin Kolman <martin.kolman@gmail.com>
2015-10-08 22:10:57 +0200

    Handle inheritence in the 2.5 property setter implementation
    The previous "backport" could not handle subclases overriding properties
    of a parent class.

--- core/backports/__init__.py
+++ core/backports/__init__.py
@@ -7,34 +7,21 @@ import sys
 if sys.version_info[:2] <= (2, 5):
     # If you need to access original built-in property(), uncomment the next line.
     # __builtin__._property = property
+
     import __builtin__
-    class property(property):
+    if not hasattr(__builtin__.property, "setter"):
+        class property(__builtin__.property):
+            __metaclass__ = type
 
-        def __init__(self, fget, *args, **kwargs):
+            def setter(self, method):
+                return property(self.fget, method, self.fdel)
 
-            self.__doc__ = fget.__doc__
-            super(property, self).__init__(fget, *args, **kwargs)
+            def deleter(self, method):
+                return property(self.fget, self.fset, method)
 
-        def setter(self, fset):
+            @__builtin__.property
+            def __doc__(self):
+                """Doc seems not to be set correctly when subclassing"""
+                return self.fget.__doc__
 
-            cls_ns = sys._getframe(1).f_locals
-            for k, v in cls_ns.iteritems():
-                if v == self:
-                    propname = k
-                    break
-            cls_ns[propname] = property(self.fget, fset,
-                                        self.fdel, self.__doc__)
-            return cls_ns[propname]
-
-        def deleter(self, fdel):
-
-            cls_ns = sys._getframe(1).f_locals
-            for k, v in cls_ns.iteritems():
-                if v == self:
-                    propname = k
-                    break
-            cls_ns[propname] = property(self.fget, self.fset,
-                                        fdel, self.__doc__)
-            return cls_ns[propname]
-
-    __builtin__.property = property
+        __builtin__.property = property
