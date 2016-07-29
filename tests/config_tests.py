3298b3e9b02d383a21699f32fa070e63c2b12680
Martin Kolman <martin.kolman@gmail.com>
2016-07-29 03:09:42 +0200

    Check if the default map and user config files are valid

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -1,9 +1,21 @@
 import os
 import unittest
+from configobj import ConfigObj
+
+MAP_CONFIG_PATH = "../data/default_configuration_files/map_config.conff"
+USER_CONFIG_PATH = "../data/default_configuration_files/user_config.conf"
 
 class BasicDefaultConfigFilesTests(unittest.TestCase):
     def default_configs_exist_test(self):
         """Check that the default configuration files exist"""
-        self.assertTrue(os.path.exists("../data/default_configuration_files/map_config.conf"))
-        self.assertTrue(os.path.exists("../data/default_configuration_files/user_config.conf"))
-        
+        self.assertTrue(os.path.exists(MAP_CONFIG_PATH))
+        self.assertTrue(os.path.exists(USER_CONFIG_PATH))
+
+    def map_config_valid_Test(self):
+        """Check if the map config is valid (can be parsed by ConfigObj)"""
+        ConfigObj(MAP_CONFIG_PATH)
+
+    def user_config_valid_Test(self):
+        """Check if the user config is valid (can be parsed by ConfigObj)"""
+        ConfigObj(USER_CONFIG_PATH)
+
