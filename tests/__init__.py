f4d4581606a344ef25d4785509c786d7e04cbb74
Martin Kolman <martin.kolman@gmail.com>
2016-07-31 03:29:02 +0200

    Redirect all log messages to stdout when running tests
    So that we can more easily see what went wrong if a test fails.

---
+++ tests/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+ch = None
+
+def redirect_logging_to_stdout():
+    global ch
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+redirect_logging_to_stdout()
+
