0b101fe701ca69099211366d52c6c7d4df56b71a
Martin Kolman <martin.kolman@gmail.com>
2022-01-02 04:23:00 +0100

    Cleanup the TracklogManager module

--- modules/mod_tracklogManager.py
+++ modules/mod_tracklogManager.py
@@ -30,55 +30,10 @@ class TracklogManager(RanaModule):
 
     def __init__(self, *args, **kwargs):
         RanaModule.__init__(self, *args, **kwargs)
-        self.scrollDict = {}
-        self.currentNumItems = 0
         self.LTModule = None
 
     def firstTime(self):
         self.LTModule = self.m.get('loadTracklogs', None)
-
-    def handleMessage(self, message, messageType, args):
-
-        if message == 'getElevation':
-            self.log.info("getting elevation info for active tracklog")
-            activeTracklog = self.LTModule.get_active_tracklog()
-            # generate a list of (lat,lon) tuples
-            latLonList = map(lambda x: (x.latitude, x.longitude), activeTracklog.trackpointsList[0])
-            # look-up elevation data using Geonames asynchronously
-            online = self.m.get("onlineServices", None)
-            if online:
-                online.elevFromGeonamesBatchAsync(latLonList, self._handleElevationLookupResults,
-                                                  'geonamesBatchResults', activeTracklog)
-
-
-        elif message == 'loadTrackProfile':
-            # get the data needed for drawing the dynamic route profile in the osd
-            track = self.LTModule.get_active_tracklog()
-            self.m.get('showOSD', None).routeProfileData = track.perElevList
-
-        elif message == 'unLoadTrackProfile':
-            self.m.get('showOSD', None).routeProfileData = None
-
-        elif message == 'askDeleteActiveTracklog':
-            ask = self.m.get('askMenu', None)
-            path = self.LTModule.get_active_tracklog_path()
-            question = "do you really want to delete:|%s|?" % path
-            yesAction = "tracklogManager:deleteActiveTracklog|set:menu:tracklogManager#tracklogManager"
-            noAction = "set:menu:tracklogManager#tracklogInfo"
-            ask.setupAskYesNo(question, yesAction, noAction)
-
-        elif message == 'deleteActiveTracklog':
-            path = self.LTModule.get_active_tracklog_path()
-            if path:
-                self.deleteTracklog(path)
-                self.set('activeTracklogPath', None)
-
-        elif message == 'setActiveTracklogToCurrentCat':
-            path = self.LTModule.get_active_tracklog_path()
-            currentCategory = self.get('currentTracCat', None)
-            if currentCategory:
-                self.log.info("changing category for:\n%s\nto %s" % path, currentCategory)
-                self.LTModule.setTracklogPathCategory(path, currentCategory)
 
     def deleteTracklog(self, path):
         # delete a tracklog
